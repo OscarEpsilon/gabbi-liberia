41cf43b6bcf8ec829ba9f86dc40beb0882e384b0
OscarEpsilon <110500526+OscarEpsilon@users.noreply.github.com>
2025-03-24 12:00:32 -0400

    More stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
 import { useState } from 'react'
 import './App.css'
 import AfMap from './components/AfMap'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <NavBar />
       <h1>Colonization and Liberia</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Info about Liberia!
         </p>
       </div>
       <AfMap />
+      <div className="quiz">
+        <div>
+          <p>
+            Example question: Question what the question?
+          </p>
+        </div>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
     </>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,8 @@
+function NavBar() {
+    return <nav className="nav">
+        <p className="titleText">Colonization and Liberia</p>
+        <p className="signature">Warren Hayman</p>
+    </nav>    
+}
+
+export default NavBar;
