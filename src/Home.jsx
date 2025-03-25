f84b39b321bb6a00bf19d199e44d33006451d4ae
OscarEpsilon <110500526+OscarEpsilon@users.noreply.github.com>
2025-03-25 00:16:20 -0400

    very little

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,31 +11,33 @@ const Home = () => {
       <h1>Liberia &mdash; America's Unique African Colony</h1>
       <div className="content">
         <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Info about Liberia!
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Info about Liberia!
+        </p>
+        <p>
+          First though, test your geographical knowledge with the questions below!
         </p>
       </div>
       <AfMap />
       <div className="quiz">
         <div>
           <p className="quizq">
-            Example question: Question what the question?
+            Question 1:  What country is highlighted in light green?
           </p>
         </div>
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => 1)}>
           count is {count}
         </button>
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => 2)}>
           count is {count}
         </button>
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => 3)}>
           count is {count}
         </button>
       </div>
       <div className="quiz">
         <div>
           <p className="quizq">
-            Example question: Question what the question?
+             Question 2:  What country is highlighted in blue?
           </p>
         </div>
         <button onClick={() => setCount((count) => count + 1)}>
