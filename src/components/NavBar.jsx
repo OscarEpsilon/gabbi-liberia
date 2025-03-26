cdb2359b1e129e0bee0975b2a3b33bc6926e91f6
OscarEpsilon <110500526+OscarEpsilon@users.noreply.github.com>
2025-03-26 17:07:22 -0400

    whoops 2

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function NavBar() {
     return <nav className="nav">
         <Link to="/">
-            <img className= "icon" src={'/libfull.png'} width='60px' height='30px'/>
+            <img className= "icon" src={'./libfull.png'} width='60px' height='30px'/>
             <p className="titleText">Colonization and Liberia</p>
         </Link>
         <p className="signature">Warren Hayman</p>
