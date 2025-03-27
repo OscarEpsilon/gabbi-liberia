82a738ef52f59cc5c70822d3274134567b87d8fc
OscarEpsilon <110500526+OscarEpsilon@users.noreply.github.com>
2025-03-26 23:53:36 -0400

    sources, baby

--- src/Sources.jsx
+++ src/Sources.jsx
@@ -8,13 +8,28 @@ const Sources = () => {
       <h1>Sources</h1>
       <div className="content">
         <p className='citation'>
-          Petterson, Donald Rahl, and Svend E. Holsoe. Liberia, Encyclopedia Britannica, March 19, 2025. https://www.britannica.com/place/Liberia. 
+          Petterson, Donald Rahl, and Svend E. Holsoe. Liberia, Encyclopedia Britannica, March 19, 2025. https://www.britannica.com/place/Liberia.
         </p>
         <p className='citation'>
-        Dunn-Marcos, R., Kollehlon, K. T., Ngovo, B., & Russ, E. (2005). Liberians: An introduction to their history and culture (D. A. Ranard, Ed.). Center for Applied Linguistics, Cultural Orientation Resource Center.
+          Dunn-Marcos, R., Kollehlon, K. T., Ngovo, B., & Russ, E. (2005). Liberians: An introduction to their history and culture (D. A. Ranard, Ed.). Center for Applied Linguistics, Cultural Orientation Resource Center.
         </p>
         <p className='citation'>
-          Correctly Cited Source
+          Beyan, Amos Jones. African American settlements in West Africa: John Brown Russwurm and the American civilizing efforts. New York City, New York: Palgrave, 2005.
+        </p>
+        <p className='citation'>
+          Burin, Eric. Slavery and the peculiar solution: A history of the American Colonization Society. Gainesville, Florida: University Press of Florida, 2008.
+        </p>
+        <p className='citation'>
+          Cheng, Christine. Extralegal groups in post-conflict Liberia: How trade makes the State. Oxford, United Kingdom: Oxford University Press, 2018.
+        </p>
+        <p className='citation'>
+          “Praise for the Woman Who Put Liberia Back on Its Feet.” The Economist, October 5, 2017. https://www.economist.com/leaders/2017/10/05/praise-for-the-woman-who-put-liberia-back-on-its-feet.
+        </p>
+        <p className='citation'>
+          “Christian Community and the Development of an Americo-Liberian Identity, 1824-1878” Wegmann, Andrew N. Louisiana State University, May 5, 2010.
+        </p>
+        <p className='citation'>
+          Moran, Mary H. Liberia: The violence of democracy. Philadelphia, Pennsylvania: University of Pennsylvania Press, 2013.
         </p>
       </div>
     </>
